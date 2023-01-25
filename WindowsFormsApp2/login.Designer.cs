e713677f30d3e3ef9fede7ae2c8072050f55b168
BasmallaMabrouk <basmallamabrouk@gmail.com>
2023-01-25 12:47:42 +0200

    add a text inside a button to login

--- WindowsFormsApp2/login.Designer.cs
+++ WindowsFormsApp2/login.Designer.cs
@@ -114,11 +114,12 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(378, 305);
+            this.button1.Location = new System.Drawing.Point(367, 305);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(127, 23);
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(177, 63);
             this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
+            this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
