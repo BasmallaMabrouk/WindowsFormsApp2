4ab87486dea8324b4a7a8e700a7f227c2fc7dd9c
BasmallaMabrouk <basmallamabrouk@gmail.com>
2023-01-25 13:14:41 +0200

    add a panel

--- WindowsFormsApp2/employees.Designer.cs
+++ WindowsFormsApp2/employees.Designer.cs
@@ -28,7 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 0;
             // 
             // employees
             // 
@@ -36,6 +44,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
             this.Name = "employees";
             this.Text = "employees";
             this.ResumeLayout(false);
@@ -43,5 +52,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
     }
 }
