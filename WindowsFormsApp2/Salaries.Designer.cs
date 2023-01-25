e9167f2cb4cc6a97986166065257df5f95cc0c27
BasmallaMabrouk <basmallamabrouk@gmail.com>
2023-01-25 16:14:22 +0200

    make a start position in centerscreen

--- WindowsFormsApp2/Salaries.Designer.cs
+++ WindowsFormsApp2/Salaries.Designer.cs
@@ -38,6 +38,7 @@
             this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Salaries";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Salaries";
             this.ResumeLayout(false);
 
