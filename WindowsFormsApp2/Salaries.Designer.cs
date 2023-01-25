12ec18d123dadc3a112970b4210b9e5930cd98d8
BasmallaMabrouk <basmallamabrouk@gmail.com>
2023-01-25 16:55:10 +0200

    rename a label for salary Amount

--- WindowsFormsApp2/Salaries.Designer.cs
+++ WindowsFormsApp2/Salaries.Designer.cs
@@ -183,9 +183,9 @@
             this.label6.ForeColor = System.Drawing.Color.Teal;
             this.label6.Location = new System.Drawing.Point(28, 384);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(144, 29);
+            this.label6.Size = new System.Drawing.Size(167, 29);
             this.label6.TabIndex = 33;
-            this.label6.Text = "Date of Birth";
+            this.label6.Text = "Salary Amount";
             // 
             // label5
             // 
