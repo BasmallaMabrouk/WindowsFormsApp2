54297925abb8abc038845cd68c01ba1da06dc5a3
BasmallaMabrouk <basmallamabrouk@gmail.com>
2023-01-25 11:52:55 +0200

    add label

--- WindowsFormsApp2/login.Designer.cs
+++ WindowsFormsApp2/login.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // panel1
@@ -41,24 +42,36 @@
             this.panel1.Size = new System.Drawing.Size(208, 1050);
             this.panel1.TabIndex = 0;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(224, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 58);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
             // login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(29F, 58F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1924, 1050);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
             this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
             this.Name = "login";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
